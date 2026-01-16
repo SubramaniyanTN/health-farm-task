fd69dfab92db351904fdbe597874b16a80cca00c
subramaniyan <subramaniyan@webizze.com>
2026-01-16 21:27:53 +0530

    added the colors

--- colors/unistylesColor.ts
+++ colors/unistylesColor.ts
@@ -1,9 +1,52 @@
-export const lightThemeColors = {
-    primary: '#000',
-    secondary: '#fff',
-}
+// themeColors.ts
 
-export const darkThemeColors = {
-    primary: '#fff',
-    secondary: '#000',
-}
+export const lightThemeColors = {
+    // Brand
+    primary: '#2E6FF3',
+    primarySoft: '#E9F0FF',
+    primaryMuted: '#C0D4FB',
+  
+    // Text
+    textPrimary: '#33384B',
+    textSecondary: '#7D8A95',
+    textMuted: '#AAB6C3',
+    textInverse: '#FFFFFF',
+  
+    // Backgrounds
+    background: '#F0F4FC',
+    surface: '#FFFFFF',
+    surfaceSecondary: '#FAFAFB',
+    surfaceMuted: '#F4F4F6',
+  
+    // Borders
+    border: '#B2BCC9',
+    borderMuted: '#FAFAFC',
+  
+    // Status
+    danger: '#ED5E5E',
+  };
+  
+  export const darkThemeColors = {
+    // Brand
+    primary: '#2E6FF3',
+    primarySoft: '#333D4D',
+    primaryMuted: '#245BCE',
+  
+    // Text
+    textPrimary: '#F1F4F6',
+    textSecondary: '#AAB6C3',
+    textMuted: '#7D8A95',
+    textInverse: '#232528',
+  
+    // Backgrounds
+    background: '#232528',
+    surface: '#2B3037',
+    surfaceSecondary: '#333D4D',
+  
+    // Borders
+    border: '#333333',
+  
+    // Status
+    danger: '#E86666',
+  };
+  
