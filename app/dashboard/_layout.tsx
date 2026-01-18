c37038df5db4a3abbeb8d77298f7365e7e58e1bf
subramaniyan <subramaniyan@webizze.com>
2026-01-18 19:45:32 +0530

    added the dashbord screen

--- app/dashboard/_layout.tsx
+++ app/dashboard/_layout.tsx
@@ -3,7 +3,8 @@ import { Stack } from "expo-router";
 export default function DashboardLayout() {
     return (
         <Stack>
-            <Stack.Screen name="index" />
+            <Stack.Screen name="index" options={{headerShown:false}} />
         </Stack>
     )
 }
+
