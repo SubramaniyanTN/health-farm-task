47226ef03f74a3ec4353fbd1293e3346a31617e1
subramaniyan <subramaniyan@webizze.com>
2026-01-19 13:13:46 +0530

    added the realtime fetch

--- Screen/Chat/Components/src/useMessageRealtime.ts
+++ Screen/Chat/Components/src/useMessageRealtime.ts
@@ -8,7 +8,7 @@ export const useMessageRealtime = (channelId?: string) => {
 
   useEffect(() => {
     if (!channelId) return;
-
+    console.log({channelId})
     const channel = subscribeToMessages(channelId, queryClient);
 
     return () => {
--- Screen/Chat/index.tsx
+++ Screen/Chat/index.tsx
@@ -6,27 +6,27 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { StyleSheet } from "react-native-unistyles";
 import { Footer, useMessageRealtime } from "./Components";
 export default function Chat() {
-    const { id } = useGlobalSearchParams<{id:string}>();
-    const {bottom}=useSafeAreaInsets()
-    const {data,isLoading}=useGetMessages(id as string)
-    console.log({data})
+    const { id } = useGlobalSearchParams<{ id: string }>();
+    const { bottom } = useSafeAreaInsets()
+    const { data, isLoading } = useGetMessages(id as string)
+    console.log({ data })
     useMessageRealtime(id);
     const RenderItem = ({ item }: { item: Message }) => {
         return <Text>{item.content}</Text>
     }
     return (
-        <View style={[styles.container,{paddingBottom:bottom}]}>
-            <FlatList data={data} renderItem={RenderItem} style={styles.list} contentContainerStyle={{flexGrow:1}} />
+        <View style={[styles.container, { paddingBottom: bottom }]}>
+            <FlatList data={data} renderItem={RenderItem} style={styles.list} contentContainerStyle={{ flexGrow: 1 }} />
             <Footer />
         </View>
     )
 }
-const styles = StyleSheet.create((theme)=>({
-    container:{
-        flex:1,
-        paddingHorizontal:10,
+const styles = StyleSheet.create((theme) => ({
+    container: {
+        flex: 1,
+        paddingHorizontal: 10,
     },
-    list:{
-        flex:1,
+    list: {
+        flex: 1,
     },
 }))
